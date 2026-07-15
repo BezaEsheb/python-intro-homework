ee4c62be5be351aff9ab4cb7c27b9acfae64f78a
Bezawit Eshete <beshete29@gmail.com>
2026-07-14 20:02:53 -0700

    Add warmup3

---
+++ week-2/assignment-2/warmup3.py
@@ -0,0 +1,7 @@
+#git log --oneline output:
+#[Beza@DESKTOP-Q2R9I93 MINGW64 ~/python-intro-homework (assignment-2)
+#$ git log --oneline
+#53ca22b (HEAD -> assignment-2) Add warmup1 and warmup2 for week 2
+#294742d (origin/main, origin/HEAD, main) add week 2 folder
+
+print("This week, I learned how to use the command line to create branches, navigate directories, and commit code directly to GitHub!")
